82c27cc4b4af81a777d1f03e9b77ef4d8a8ec84c
Jonathan Azoff <jon@azoffdesign.com>
2012-08-15 03:09:01 -0700

    updated client styles

--- js/famous.js
+++ js/famous.js
@@ -2,10 +2,14 @@
 
 	"use strict";
 
-	var form, info, config, submit;
+	var form, info, client, config, submit;
 
 	function authorizeStripe(config) {
 		stripe.setPublishableKey(config.stripeKey);
+	}
+
+	function formReady() {
+		form.addClass('ready');
 	}
 
 	function setGlobals() {
@@ -17,32 +21,67 @@
 
 	function addListeners() {
 		config.then(authorizeStripe);
+		config.then(formReady);
 		form.on('click', 'a', toggleForm);
 		form.on('submit', getToken);
 	}
 
-	function getParams() {
+	function formParams() {
 		var params = {};
 		var inputs = form.serializeArray();
 		$.each(inputs, function(i, input){
 			params[input.name] = input.value;
 		});
+		return params;
 	}
 
 	function getToken() {
+		notify();
+		var params = formParams();
 		submit.prop('disabled', true);
 		form.addClass('submitting');
-		info.removeClass('fail pass');
-		stripe.createToken(getParams(), handleResponse);
+		stripe.createToken(params, responseHandler(params));
 		return false;
 	}
 
-	function handleResponse(status, response) {
+	function notify(msg, className) {
+		info.text(msg)
+			.toggleClass('fail', className === 'fail')
+			.toggleClass('pass', className === 'pass');
+		submit.prop('disabled', false);
+	}
+
+	function checkResponse(response) {
 		if (response.error) {
-			info.text(response.error.message).addClass('fail');
-			submit.prop('disabled', false);
+			notify(response.error, 'fail');
+		} else if (response.content) {
+			doc.body.innerHTML = response.content;
 		} else {
-			//TODO: AJAX charge!
+			notify('You Win! Please reload the page.', 'pass');
+		}
+	}
+
+	function notifyFailure(request, code, error) {
+		notify('Server Error: ' + error, 'fail');
+	}
+
+	function chargesLeapfrog(config, token, params) {
+		return $.getJSON(config[0].apiServer + '/charges.leapfrog?callback=?', {
+			amount: params.charge*100,
+			token: token,
+			description: params.description
+		}).then(checkResponse).fail(notifyFailure);
+	}
+
+	function responseHandler(params) {
+		return function(status, token) {
+			if (token.error) {
+				notify(token.error.message, 'fail');
+			} else if(token.id) {
+				$.when(config, token.id, params).then(chargesLeapfrog);
+			} else {
+				notify('Unable to process payment at this time', 'fail');
+			}
 		}
 	}
 
